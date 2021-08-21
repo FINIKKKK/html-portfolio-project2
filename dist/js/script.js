756bd2c61ad3b7fb028d4acdd90cc2609684606c
DIGIKRASH <79862227+digitalKrash@users.noreply.github.com>
2021-08-21 15:24:00 +0500

    Начало2

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,34 +18,23 @@ $(window).scroll(() => {
 
 $(document).ready(function () {
     
-    $(function () {
-        $('.accordion').find('.accordion__item-header').click(function () {
-
-            $('.accordion__item').removeClass('accordion__item--active');
-            
-            $(this).next().slideDown('fast');
-            
-            $('.accordion__item-answer').not($(this).next()).slideUp('slow');
-            
-            $(this).parent().addClass('accordion__item--active');
-        });
-    });
-
-
-    $("body").on('click', '[href*="#"]', function (e) {
-        var fixed_offset = 100;
-        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
-        e.preventDefault();
-    });
+    $('.offers__items').slick({
+        nextArrow: '<div class="next"><svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.5833 11.75L16.822 14.5112L25.7912 23.5L16.822 32.4888L19.5833 35.25L31.3333 23.5L19.5833 11.75Z" fill="#555555"/></svg></div>',
+        prevArrow: '<div class="prev"><svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.5833 11.75L16.822 14.5112L25.7912 23.5L16.822 32.4888L19.5833 35.25L31.3333 23.5L19.5833 11.75Z" fill="#555555"/></svg></div>',
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    arrows: false,
+                }
+            },
+        ]
+    })
 
 
     $('.hamburger').on('click', function () {
-        $('.header').toggleClass('mobile');
+        $('.header').toggleClass('mobile')
     })
-
-    $('.gamb-btn').on('click', function () {
-        $('.header-mobile').toggleClass('btn--active');
-    });
 
 
 });
