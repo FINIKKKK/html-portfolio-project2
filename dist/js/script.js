8bdc50cd2afc477d347a787236bc8c7064af2ea4
DIGIKRASH <79862227+digitalKrash@users.noreply.github.com>
2021-08-25 12:17:05 +0500

    Последняя полировка
    Кое-где изменен текст, картинки, стили. Также сделан адаптив для фона у "offers". Также для упрощения кода элемент "product" сделан как отдельный файл.

--- dist/js/script.js
+++ dist/js/script.js
@@ -23,6 +23,7 @@ break;default:I+=" ui-datepicker-group-middle",T=""}I+="'>"}for(I+="<div class='
 
 
 // Main-JS -----------------
+// --- Header-Mini при скролле
 $(window).scroll(() => {
     var windowTop = $(window).scrollTop();
     windowTop > 200 ? $('.header').addClass('header-mini') : $('.header').removeClass('header-mini');
@@ -30,12 +31,36 @@ $(window).scroll(() => {
 
 
 
+// --- Hamburger
+$('.hamburger').on('click', function () {
+    $('.header').toggleClass('mobile')
+})
+
+
+
+// --- Slider "Offers"
+$('.offers__items').slick({
+    nextArrow: '<div class="next"><svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.5833 11.75L16.822 14.5112L25.7912 23.5L16.822 32.4888L19.5833 35.25L31.3333 23.5L19.5833 11.75Z" fill="#555555"/></svg></div>',
+    prevArrow: '<div class="prev"><svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.5833 11.75L16.822 14.5112L25.7912 23.5L16.822 32.4888L19.5833 35.25L31.3333 23.5L19.5833 11.75Z" fill="#555555"/></svg></div>',
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                arrows: false,
+            }
+        },
+    ]
+})
+
+
+
+// --- Диапазон цен
 $(function () {
     $("#slider-range").slider({
         range: true,
         min: 0,
         max: 500,
-        values: [75, 300],
+        values: [0, 300],
         slide: function (event, ui) {
             $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
         }
@@ -45,39 +70,16 @@ $(function () {
 });
 
 
-$(document).ready(function () {
 
-    $('.offers__items').slick({
-        nextArrow: '<div class="next"><svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.5833 11.75L16.822 14.5112L25.7912 23.5L16.822 32.4888L19.5833 35.25L31.3333 23.5L19.5833 11.75Z" fill="#555555"/></svg></div>',
-        prevArrow: '<div class="prev"><svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.5833 11.75L16.822 14.5112L25.7912 23.5L16.822 32.4888L19.5833 35.25L31.3333 23.5L19.5833 11.75Z" fill="#555555"/></svg></div>',
-        responsive: [
-            {
-                breakpoint: 600,
-                settings: {
-                    arrows: false,
-                }
-            },
-        ]
-    })
-
-
-    $('.hamburger').on('click', function () {
-        $('.header').toggleClass('mobile')
-    })
-
-
-    var i = $(".cart__counter-number").val();
-    $(".cart__counter--plus").click(function () {
-        $(".cart__counter-number").val(++i);
-    });
-    $(".cart__counter--minus").click(function () {
-        $(".cart__counter-number").val(--i);
-    });
-
-
-
-
+// --- Счетчик в козине
+var i = $(".cart__counter-number").val();
+$(".cart__counter--plus").click(function () {
+    $(".cart__counter-number").val(++i);
 });
+$(".cart__counter--minus").click(function () {
+    $(".cart__counter-number").val(--i);
+});
+
 
 ;
 
